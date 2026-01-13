50ce8e51f8356a84f9a797aa6c359ca12aceab71
Johnathan MEUNIER <johnathan2693@gmail.com>
2026-01-13 14:13:05 +0100

    refactor(Router): data mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import { Layout } from "@/layout/Layout";
-
-import { GameOfLifePage } from "@pages/GameOfLife";
-import { PokedexPage } from "@pages/Pokedex";
-import { PokemonPage } from "@pages/Pokemon";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
-
+import { RouterProvider } from "react-router";
+import routes from "./routes";
 function App() {
-  return (
-    <Router>
-      <main>
-        <div className="router">
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<Navigate to="/pokedex" replace />} />
-              <Route path="/pokedex" element={<PokedexPage />} />
-              <Route path="/pokemon" element={<PokemonPage />} />
-              <Route path="/game-of-life" element={<GameOfLifePage />} />
-              <Route path="*" element={<div>404 Not Found</div>} />
-            </Route>
-          </Routes>
-        </div>
-      </main>
-    </Router>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
---
+++ src/routes.ts
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import { Layout } from "@/layout/Layout";
+import { GameOfLifePage } from "@pages/GameOfLife";
+import { PokedexPage } from "@pages/Pokedex";
+import { PokemonPage } from "@pages/Pokemon";
+import { NotFoundPage } from "@pages/NotFound";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      { path: "/pokedex", Component: PokedexPage },
+      { path: "/pokemon", Component: PokemonPage },
+      { path: "/game-of-life", Component: GameOfLifePage },
+      { path: "*", Component: NotFoundPage },
+    ],
+  },
+]);
+
+export default routes;
