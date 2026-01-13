6027e38b44434edcd6d2cabf8fea637dc83739e9
Johnathan MEUNIER <johnathan2693@gmail.com>
2026-01-13 16:06:21 +0100

    feat(Pokedex): previous & next

--- src/pages/Pokedex/Pokedex.tsx
+++ src/pages/Pokedex/Pokedex.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 
-const fetchPokemons = async () => {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+const LIMIT_PAGE = 20;
+
+const fetchPokemons = async ({ offset }: { offset: number }) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${LIMIT_PAGE}`);
   if (response.ok) {
     const data = await response.json();
     return data.results;
@@ -11,15 +13,27 @@ const fetchPokemons = async () => {
 
 export const PokedexPage = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [current_page, setCurrentPage] = useState(0);
+  const offset = current_page * LIMIT_PAGE;
 
   useEffect(() => {
-    fetchPokemons()
+    fetchPokemons({ offset })
       .then((data) => setPokemons(data))
       .catch((error) => console.error(error));
-  }, []);
+  }, [current_page, offset]);
+
+  const can_go_back = current_page > 0;
+  const can_go_next = (current_page + 1) * LIMIT_PAGE < 150;
 
   return (
     <div>
+      <button onClick={() => setCurrentPage(current_page - 1)} disabled={!can_go_back}>
+        {" "}
+        Précédent
+      </button>
+      <button onClick={() => setCurrentPage(current_page + 1)} disabled={!can_go_next}>
+        Suivant
+      </button>
       <ul>
         {pokemons.map((pokemon: { name: string; url: string }) => (
           <li key={pokemon.name}>{pokemon.name}</li>
