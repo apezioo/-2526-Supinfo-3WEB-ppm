596e8e381c119926b54fb00d80550b6b60626187
Johnathan MEUNIER <johnathan2693@gmail.com>
2026-01-13 15:18:56 +0100

    feat(Pokedex): retrieve 20 first pkm

--- src/pages/Pokedex/Pokedex.tsx
+++ src/pages/Pokedex/Pokedex.tsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from "react";
+
+const fetchPokemons = async () => {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+  if (response.ok) {
+    const data = await response.json();
+    return data.results;
+  }
+  throw new Error("Failed to fetch pokemons");
+};
+
 export const PokedexPage = () => {
-  return <h3>Contenu de ma page pokedex</h3>;
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetchPokemons()
+      .then((data) => setPokemons(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {pokemons.map((pokemon: { name: string; url: string }) => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
