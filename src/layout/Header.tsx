2e7a5c04dd1f2993b702ef0703a0b2fb1c58fe9b
Johnathan MEUNIER <johnathan2693@gmail.com>
2026-01-13 14:39:34 +0100

    feat(Router): header information

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,18 @@
-const Header = () => (
-  <header>
-    <h1>Supinfo - 2526 - 3WEB</h1>
-    <h2>TITRE DE MA PAGE</h2>
-  </header>
-);
+const titles: Record<string, string> = {
+  pokedex: "Pokedex",
+  gameoflife: "Game of Life",
+  pokemon: "Pokemon Detail",
+};
+
+const Header = () => {
+  const title = titles[window.location.pathname.substring(1)];
+
+  return (
+    <header>
+      <h1>Supinfo - 2526 - 3WEB</h1>
+      <h2>{title}</h2>
+      <title>{title}</title>
+    </header>
+  );
+};
 export default Header;
